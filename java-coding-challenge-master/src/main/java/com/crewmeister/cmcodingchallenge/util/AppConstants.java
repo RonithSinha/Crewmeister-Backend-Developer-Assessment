68d163f4a243a79d8a14991b1ac3690ed0a88f21
Ronith Sinha <ronithsinha12@gmail.com>
2025-06-10 15:54:31 +0200

    Removed unnecessary constants

--- java-coding-challenge-master/src/main/java/com/crewmeister/cmcodingchallenge/util/AppConstants.java
+++ java-coding-challenge-master/src/main/java/com/crewmeister/cmcodingchallenge/util/AppConstants.java
@@ -1,12 +1,6 @@
 package com.crewmeister.cmcodingchallenge.util;
 
 public final class AppConstants {
-
-	public static final String BASE_CURRENCY = "EUR";
-	public static final String CACHE_NAME_EXCHANGE_RATES = "exchangeRatesCache";
-	public static final String DATE_FORMAT = "yyyy-MM-dd";
-	public static final String ERROR_INVALID_DATE = "Date cannot be a current or future date.";
-
 	public static final String PAST_DATE_WARNING = "Date must be in the past; current or future dates are not allowed.";
 	public static final String CURRENCY_CACHE = "currencyCache";
 	public static final String LOAD_FAILURE_MSG = "Failed to load";
